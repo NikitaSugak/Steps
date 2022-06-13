6ff25ba5b29c3f6f1f1c947f875b823d445b023c
Nikita Sugak <sugak.nik@gmail.com>
2022-06-13 18:37:41 +0300

    first commit

---
+++ TestTask/Model/Step.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Model
+{
+    internal class Step : INotifyPropertyChanged
+    {
+        private string user;
+        private int averageOfSteps;
+
+        public string User
+        {
+            get { return user; }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+        public int AverageOfSteps
+        {
+            get { return averageOfSteps; }
+            set
+            {
+                averageOfSteps = value;
+                OnPropertyChanged("Avg of steps");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestTask/ViewModel/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Model;
+
+namespace TestTask.ViewModel
+{
+    internal class Main : INotifyPropertyChanged
+    {
+        private Step selectedUser;
+
+        public ObservableCollection<Step> Steps { get; set; }
+        public Step SelectedUser
+        {
+            get { return selectedUser; }
+            set
+            {
+                selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+        public Main()
+        {
+            Steps = getCollectionUsers();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public ObservableCollection<Step> getCollectionUsers()
+        {
+            ObservableCollection<Step> users = new ObservableCollection<Step>();
+
+            users.Add(new Step { User = "kojj", AverageOfSteps = 756 });
+            users.Add(new Step { User = "konibujj", AverageOfSteps = 8756 });
+
+            return users;
+        }
+    }
+}
